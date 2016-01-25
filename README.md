c550ed6e3b28a92334d3be383eea2dcbae928ed8
Juhani Imberg <juhani@imberg.fi>
2016-01-26 00:14:17 +0200

    Generate the `index.html`
    `index.html` is now sourced from `index.jade` and `data.yaml`.

---
+++ README.md
@@ -0,0 +1,8 @@
+how 2 render
+================
+
+1. get node and nmp
+2. `sudo npm install -g gulp`
+3. `cd pieso.me`
+4. `npm install`
+5. `gulp`
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var jade = require('gulp-jade');
+var yaml = require('js-yaml');
+var fs = require('fs');
+var commonmark = require('commonmark');
+
+var reader = new commonmark.Parser();
+var writer = new commonmark.HtmlRenderer();
+
+function md(text) {
+    var parsed = reader.parse(text);
+    return writer.render(parsed);
+}
 
 gulp.task('stylus', function () {
     gulp.src('./style.styl')
@@ -7,8 +19,18 @@ gulp.task('stylus', function () {
         .pipe(gulp.dest('css'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch("./style.styl", ["stylus"]);
+gulp.task('jade', function () {
+    var data = yaml.safeLoad(fs.readFileSync('./data.yaml', 'utf8'));
+    data.md = md;
+    gulp.src('./index.jade')
+        .pipe(jade({locals: data}))
+        .pipe(gulp.dest('./'));
 });
 
-gulp.task('default', ['watch', 'stylus']);
+gulp.task('watch', function() {
+    gulp.watch('./style.styl', ['stylus']);
+    gulp.watch('./index.jade', ['jade']);
+    gulp.watch('./data.yaml', ['jade']);
+});
+
+gulp.task('default', ['watch', 'stylus', 'jade']);
